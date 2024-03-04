a343e9e68a723b0f560ceed15d869437cf4b0d3c
gai <115063721+gai1637@users.noreply.github.com>
2024-03-04 14:33:48 +0900

    3/4 14::33

--- StageScene.h
+++ StageScene.h
@@ -1,8 +1,15 @@
 ﻿#pragma once
 #include "IScene.h"
+#include "ViewProjection.h"
+#include"FollowCamera.h"
+#include<memory>
 class StageScene : public IScene {
+public:
 	void Iint() override;
 	void Update() override;
 	void Draw3D() override;
 	void Draw2D() override;
+
+private:
+
 };
